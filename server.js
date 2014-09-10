041841194d90874d29286e03fbe53ec4658cbfe0
Cato Skogholt <cskogholt@gmail.com>
2014-09-10 14:14:10 +0200

    Ready do commit to Heroku

--- server.js
+++ server.js
@@ -26,7 +26,12 @@ app.use(stylus.middleware(
 ));
 app.use(express.static(__dirname + '/public'));
 
-mongoose.connect('mongodb://localhost/multivision');
+if (env === 'development') {
+    mongoose.connect('mongodb://localhost/multivision');
+} else {
+    mongoose.connect('mongodb://cato:monzter1@ds035260.mongolab.com:35260/multivision_cato');
+}
+
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error...'));
 db.once('open', function callback() {
@@ -49,6 +54,6 @@ app.get('*', function (req, res) {
     });
 });
 
-var port = 3030;
+var port = process.env.PORT || 3030;
 app.listen(port);
 console.log('Listening on port ' + port + '...');
