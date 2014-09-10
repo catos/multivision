020b210a5d2cece3fd7648779fd7547473b4cd17
Cato Skogholt <cskogholt@gmail.com>
2014-09-10 13:41:22 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,52 @@
+var express = require('express'),
+    stylus = require('stylus'),
+    logger = require('morgan'),
+    bodyParser = require('body-parser')
+    mongoose = require('mongoose');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var app = express();
+
+function compile(str, path) {
+    return stylus(str).set('filename', path);
+};
+
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(bodyParser());
+app.use(stylus.middleware(
+    {
+        src: __dirname + '/public',
+        compile: compile
+    }
+));
+app.use(express.static(__dirname + '/public'));
+
+mongoose.connect('mongodb://localhost/multivision');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+    console.log('multivision db openened');
+})
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc) {
+   mongoMessage = messageDoc.message;
+});
+
+app.get('/partials/:partialPath', function(req, res) {
+    res.render('partials/' + req.params.partialPath);
+});
+
+app.get('*', function (req, res) {
+    res.render('index', {
+        mongoMessage: mongoMessage
+    });
+});
+
+var port = 3030;
+app.listen(port);
+console.log('Listening on port ' + port + '...');
