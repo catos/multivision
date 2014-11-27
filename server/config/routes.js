6fc23d052e6d877ab753cdf5878d88cc7248e349
Cato Skogholt <cskogholt@gmail.com>
2014-11-27 15:26:56 +0100

    Persistent login

--- server/config/routes.js
+++ server/config/routes.js
@@ -13,7 +13,9 @@ module.exports = function(app) {
     });
 
     app.get('*', function (req, res) {
-        res.render('index');
+        res.render('index', {
+            bootstrappedUser: req.user
+        });
     });
 
 };
